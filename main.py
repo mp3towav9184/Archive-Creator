ba06e53e83149bd40dcc80d8d803870e87c84aa5
zarir94 <shahriar@gmail.com>
2024-06-09 02:18:15 -0700

    Update

--- main.py
+++ main.py
@@ -5,20 +5,22 @@ from accManager import *
 pwd = 'Strasburger@'+str(randint(11111, 99999))
 
 opt = ChromiumOptions()
-opt.add_extension('./hrsf_ext')
+opt.add_extension('./bcs_ext')
 opt.set_argument('--start-maximized')
 opt.auto_port()
 
 page = ChromiumPage(opt)
-sleep(5)
-page.get('chrome-extension://enkaklgnonabiokamjcfdchgfjfchfmj/popup/popup.html')
-sleep(5)
 page.get('https://archive.org/account/signup')
 
 page.ele('css:[name="username"]').input(getMail())
-page.ele('css:[name="screenname"]').input(f'HOOC-COOH{randint(111111111, 999999999)}')
+page.ele('css:[name="screenname"]').input(f'HOOC-COOH-{randint(111111111, 999999999)}')
 page.ele('css:[name="password"]').input(pwd)
 page.ele('css:[name="submit-to-signup"]').click()
+
+iframe = page.get_frame(page.ele('css:iframe[title="recaptcha challenge expires in two minutes"]'))
+sr = iframe.ele('css:.help-button-holder').shadow_root
+sb = sr.ele('css:#solver-button')
+sb.click()
 
 page.get(getLink())
 sleep(3)
